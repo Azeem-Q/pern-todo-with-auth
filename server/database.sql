8bace47e509b2ef2f0fe8ee62e25c38eacfba60d
Azeem <livelyazeem@gmail.com>
2023-02-03 08:11:43 +0500

    learning pern auth

--- server/database.sql
+++ server/database.sql
@@ -7,3 +7,17 @@ CREATE Table
         user_email VARCHAR(255) NOT NULL,
         user_password VARCHAR(255) NOT NULL
     );
+
+--insert fake users
+
+INSERT INTO
+    users(
+        user_name,
+        user_email,
+        user_password
+    )
+VALUES (
+        'henry',
+        'henryly213@gmail.com',
+        'kthl8822'
+    );
---
+++ server/db.js
@@ -0,0 +1,12 @@
+const Pool = require("pg").Pool;
+require("dotenv").config();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: "localhost",
+  port: 5432,
+  database: "jwtauth",
+});
+
+module.exports = pool;
