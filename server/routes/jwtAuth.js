4c44e345f74d9dad52f6e6131ab21790082b73bd
Azeem <livelyazeem@gmail.com>
2023-02-04 08:45:27 +0500

    learning pern auth

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -1,6 +1,7 @@
 const pool = require("../db");
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
+const jwtGenerator = require("../utils/jwtGenerator");
 
 // registering
 
@@ -31,9 +32,47 @@ router.post("/register", async (req, res) => {
       [name, email, bcryptPassword]
     );
 
-    res.json(newUser.rows[0])
-
     // 5. generating jwt token
+
+    const token = jwtGenerator(newUser.rows[0].user_id);
+    res.json(token);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    // 1. destructure the req.body
+
+    const { email, password } = req.body;
+
+    // 2. check if user doesn't exist (if not then throw error)
+
+    const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
+      email,
+    ]);
+
+    if (user.rows.length === 0) {
+      return res.status(401).json("Password or Email is incorrect");
+    }
+
+    // 3. check if incoming password matches with the password in database
+
+    const validPassword = await bcrypt.compare(
+      password,
+      user.rows[0].user_password
+    );
+
+    if (!validPassword) {
+      return res.status(401).json("Password or Email is incorrect");
+    }
+
+    // 4. issue the jwt token
+
+    const token = jwtGenerator(user.rows[0].user_id);
+    res.json(token);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
