b5a463fa0a7c3c2c58317c4411001b996d114338
Azeem <livelyazeem@gmail.com>
2023-02-09 08:42:57 +0500

    learning pern auth

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import React, { Fragment } from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+// components
+
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+function App() {
+  return (
+    <Fragment>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/login"
+              render={(props) => <Login {...props} />}
+            />
+            <Route
+              exact
+              path="/register"
+              render={(props) => <Register {...props} />}
+            />
+            <Route
+              exact
+              path="/dashboard"
+              render={(props) => <Dashboard {...props} />}
+            />
+          </Routes>
+        </div>
+      </Router>
+    </Fragment>
+  );
+}
+
+export default App;
